ca1004bf20505484cd00a26f496557d72d26cafe
MuddyGames <make@muddygames.com>
2022-06-08 04:37:31 +0100

    Spinebox Working in Build

--- src/scripts/scenes/gameScene.ts
+++ src/scripts/scenes/gameScene.ts
@@ -5,7 +5,15 @@ import FpsText from '../objects/fpsText'
 const SPINEBOY_KEY = 'spineboy'
 
 export default class GameScene extends Phaser.Scene {
+
   fpsText
+
+  private spineBoy!: SpineGameObject
+	private cursors!: Phaser.Types.Input.Keyboard.CursorKeys
+	private animNameLabel!: Phaser.GameObjects.Text
+	
+	private animationNames: string[] = []
+	private animationIndex = 0
 
   constructor() {
     super({ key: 'GameScene' })
@@ -13,7 +21,7 @@ export default class GameScene extends Phaser.Scene {
 
   preload(){
     this.load.setPath('assets/spine/')
-		//this.load.spine(SPINEBOY_KEY, 'spineboy.json', 'spineboy.atlas')
+		this.load.spine(SPINEBOY_KEY, 'spineboy-pro.json', 'spineboy-pro.atlas')
 	}
 
   create() {
@@ -28,11 +36,72 @@ export default class GameScene extends Phaser.Scene {
       })
       .setOrigin(1, 0)
 
-      //this.add.spine(400, 600, SPINEBOY_KEY, 'idle', true)
+      const startAnim = 'idle'
+
+      this.spineBoy = this.createSpineBoy(startAnim)
+      this.cursors = this.input.keyboard.createCursorKeys()
+      this.animNameLabel = this.add.text(400, 100, startAnim, { color: '#fff' })
+  
+      this.initializeAnimationsState(this.spineBoy)
   }
 
   update() {
     this.fpsText.update()
-    //this.scene.start('MainScene')
+    const size = this.animationNames.length
+		if (Phaser.Input.Keyboard.JustDown(this.cursors.right!))
+		{
+			if (this.animationIndex >= size - 1)
+			{
+				this.animationIndex = 0
+			}
+			else
+			{
+				++this.animationIndex
+			}
+
+			this.changeAnimation(this.animationIndex)
+		}
+		else if (Phaser.Input.Keyboard.JustDown(this.cursors.left!))
+		{
+			if (this.animationIndex <= 0)
+			{
+				this.animationIndex = size - 1
+			}
+			else
+			{
+				--this.animationIndex
+			}
+
+			this.changeAnimation(this.animationIndex)
+		}
   }
+
+  private createSpineBoy(startAnim = 'idle')
+	{
+		const spineBoy = this.add.spine(400, 600, SPINEBOY_KEY, startAnim, true)
+
+		spineBoy.scaleX = 0.5
+		spineBoy.scaleY = 0.5
+
+		return spineBoy
+	}
+  private initializeAnimationsState(spineGO: SpineGameObject)
+	{
+		const startAnim = spineGO.getCurrentAnimation().name
+
+		spineGO.getAnimationList().forEach((name, idx) => {
+			this.animationNames.push(name)
+			if (name === startAnim)
+			{
+				this.animationIndex = idx
+			}
+		})
+	}
+
+	private changeAnimation(index: number)
+	{
+		const name = this.animationNames[index]
+		this.spineBoy.play(name, true)
+		this.animNameLabel.text = name
+	}
 }
