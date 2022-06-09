9bb5ea548bb9e2490ededc6a9cf1659a4bf00954
RossWalkerBurke <58166708+RossWalkerBurke@users.noreply.github.com>
2022-06-09 10:12:12 +0100

    addding to assets/update
    I've created a new map with the new tile set and added a new d-pad.

---
+++ src/assets/img/Updating/tileset_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="map_1" tilewidth="32" tileheight="32" tilecount="3588" columns="52">
+ <image source="map_1.png" width="1668" height="2224"/>
+</tileset>
