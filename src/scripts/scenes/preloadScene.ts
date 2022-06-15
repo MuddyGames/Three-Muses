641a5c6cee9452ed61fa817f093bf41416cd3284
MuddyGames <make@muddygames.com>
2022-06-15 08:12:31 +0100

    Build

--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -49,8 +49,8 @@ export default class PreloadScene extends Phaser.Scene {
   }
 
   create() {
-    this.scene.start('SplashScene')
-    //this.scene.start('GameScene')
+    //this.scene.start('SplashScene')
+    this.scene.start('GameScene')
 
     /**
      * This is how you would dynamically import the mainScene class (with code splitting),
